9df77ed56861bca0a6ab0e7a5e38760961e25188
Raro Macias <ggmacias@gmail.com>
2022-10-17 11:19:56 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
  const handleClick = () => {
   setSquares(['', '', '', '', '', '', '', '', '']);
   setPlayer(true)
- }
+ };
  const calculateWinner = (arr) => {
          const lines = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]]
          for (let i = 0; i < lines.length; i++) {
@@ -25,7 +25,7 @@ function App() {
           }
       }
       return "Who will win?";
-  }
+  };
          
         
  
